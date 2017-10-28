ce834d8f7fabb89ffe376043eca6bf9f565f0480
XemoTangch <jmphper@163.com>
2017-10-28 23:47:19 +0800

    add HeroDetailComponent

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero/hero';
+
+@Component({
+    selector: 'app-hero-detail',
+    templateUrl: './hero/hero-detail.component.html',
+    styleUrls: ['./hero/hero-detail.component.css']
+})
+
+
+export class HeroDetailComponent {
+    @Input() hero: Hero;
+}
+
+//
+// import { Component, Input } from '@angular/core';
+//
+// import { Hero } from './hero/hero';
+// @Component({
+//     selector: 'hero-detail',
+//     template: `
+//     <div *ngIf="hero">
+//       <h2>{{hero.name}} details!</h2>
+//       <div><label>id: </label>{{hero.id}}</div>
+//       <div>
+//         <label>name: </label>
+//         <input [(ngModel)]="hero.name" placeholder="name"/>
+//       </div>
+//     </div>
+//   `
+// })
+// export class HeroDetailComponent {
+//     @Input() hero: Hero;
+// }
+
