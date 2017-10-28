8959a2c42e1066ef62c4a998b30a599325c9397a
XemoTangch <jmphper@163.com>
2017-10-28 19:29:55 +0800

    hero list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+// const 必须放在@Compoent上面
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+// @Component下面只能放一个类？
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Tour of Heroes';
+    title = '英雄之旅';
+    selectedHero: Hero;
+    heroes = HEROES;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
