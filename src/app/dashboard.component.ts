7e40eb82cfb3fd4b0364fd02b1b6969728e5c524
XemoTangch <jmphper@163.com>
2017-12-27 23:09:37 +0800

    no!

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -13,6 +13,6 @@ export class DashboardComponent implements OnInit {
     constructor(private heroService: HeroService) {}
     ngOnInit(): void {
         this.heroService.getHeroes()
-            .then(heroes => this.heroes = heroes.slice(1, 5)); // 使用Array.slice方法提取了第2,3,4,5个
+            .then(heroes => this.heroes = heroes.slice(1, 5)); // 使用Array.slice方法提取了第2,3,4,5个 no1
     }
 }
